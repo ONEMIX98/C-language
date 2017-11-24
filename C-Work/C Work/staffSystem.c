35926e0c942834d0ec0b678bc116ae770e091523
onemix <531784086@qq.com>
2017-11-24 21:56:47 +0800

    refined C work of chapter 7

--- C-Work/C Work/staffSystem.c
+++ C-Work/C Work/staffSystem.c
@@ -1,44 +1,75 @@
+/*职工系统：
+1. 输入10个职工信息（姓名和职工号);
+2. 按职工号排序；
+3. 通过输入的职工号输出对应职工姓名。*/
+
 #include<stdio.h>
 #include<string.h>
-#define persons 3
+#define persons 10
+#define MAX 20
 struct Member {
-	char name[20];
-	char id[20];
-}staff[persons];
+	char name[MAX];
+	char id[MAX];
+}staff[persons], temp;
 
 void input();
 void sort();
 int find(char *num);
+void swap(char *a, char *b);
 
 void main() {
-	char *num;
+	char num[MAX]; /*用于输入查找职工号的字符串*/
+	int i;
 	input();
 	sort();
-	printf("输入你要查找职工的职工号：");
-	gets(num);
-	printf("%s", staff[find(num)].name);
+	for ( i = 0; i < persons; i++) {
+		printf("%s %s\n", staff[i].id, staff[i].name);
 
+	}
+	printf("输入你要查找职工的职工号：");
+	scanf_s("%s", num, MAX);
+	getchar();
+	if ((i = find(num))!=-1) {
+		printf("%s", staff[i].name);
+	}
+	else printf("没有找到！");
+	
 	getchar();
 }
 
 void input() {
-	printf("输入%d个职工的姓名和职工号，姓名和工号用空格隔开，各职工之间回车相隔：\n", persons);
+	printf("输入%d个职工的职工号和姓名，工号和姓名用空格隔开，各职工之间回车相隔：\n", persons);
 	for (int i = 0; i < persons; i++) {
-		scanf_s("%s %s", staff[i].name, staff[i].id);
+		scanf_s("%s %s", staff[i].id, MAX, staff[i].name, MAX);
 	}
 }
+/*选择排序法将结构数组排序*/
 void sort() {
 	int i, j, k;
-	char temp[20];
 	for (i = 0; i < persons-1; i++) {
 		k = i;
-		for (j = i + 1; i < persons; j++) {
-			if (strcmp(staff[i].id, staff[j].id) > 0) k = j;
+		for (j = i + 1; j < persons; j++) {
+			if (strcmp(staff[k].id, staff[j].id) > 0) k = j;
 		}
 		if (i != k) {
-			strcpy(temp, staff[i].id);
-			strcpy(staff[i].id, staff[j].id);
-			strcpy(staff[j].id, temp);
+			/*交换两个结构数组中的字符串*/
+			temp = staff[i];
+			staff[i] = staff[k];
+			staff[k] = temp;
 		}
 	}
 }
+/*找到指定字符串在结构数组中的位置*/
+int find(char *num) {
+	int left, mid, right;
+	left = 0;
+	right = persons-1;
+	
+	while (left <= right) {
+		mid = (left + right) / 2;
+		if (strcmp(num, staff[mid].id) > 0) left = mid + 1;
+		else if (strcmp(num, staff[mid].id) < 0) right = mid - 1;
+		else return mid;
+	}
+	return -1;
+} 
